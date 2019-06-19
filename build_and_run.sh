cdb72c8c774060452fcaea2358a5bac160f1bb2f
Jesper Jeeninga <jeeninga@gmail.com>
2019-06-19 20:37:30 +0200

    Add backup to build'n run

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 docker volume create pennmush && \
+docker volume create pennmush-backup && \
 docker build -t pm-builder builder/. && \
 docker run --rm -v pennmush:/tmp/volume pm-builder && \
 docker build -t pm-runner runner/. && \
-docker run -d -p 4201:4201 -v pennmush:/home/pennmush/game --name pennmush pm-runner
+docker build -t pm-backup backup/. && \
+docker run --rm -dit -p 4201:4201 -v pennmush:/home/pennmush/game --name pennmush pm-runner && \
+docker run --rm -dit -v pennmush:/source -v pennmush-backup:/target --name pennmush-backup pm-backup
