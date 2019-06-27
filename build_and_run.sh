b43375e2b2f00512768af5c54e00aff3dd2c1324
Jesper Jeeninga <jeeninga@gmail.com>
2019-06-27 22:51:40 +0200

    Add variables for ports and name

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
-docker volume create pennmush && \
-docker volume create pennmush-backup && \
+NAME=${1:-penn}
+PORT=${2:-4201}
+docker volume create ${NAME}mush && \
+docker volume create ${NAME}mush-backup && \
 docker build -t pm-builder builder/. && \
-docker run --rm -v pennmush:/tmp/volume pm-builder && \
+docker run --rm -v ${NAME}mush:/tmp/volume pm-builder && \
 docker build -t pm-runner runner/. && \
 docker build -t pm-backup backup/. && \
-docker run --rm -dit -p 4201:4201 -v pennmush:/home/pennmush/game --name pennmush pm-runner && \
-docker run --rm -dit -v pennmush:/source -v pennmush-backup:/target --name pennmush-backup pm-backup
+docker run -dit -p ${PORT}:4201 -v ${NAME}mush:/home/pennmush/game --name ${NAME}mush pm-runner && \
+docker run --rm -dit -v ${NAME}mush:/source -v ${NAME}mush-backup:/target --name ${NAME}mush-backup pm-backup
